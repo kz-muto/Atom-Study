f2788d5e7b95ab52ece2ae7efb9eab503c8add54
mutoch <89289398+mutoch@users.noreply.github.com>
2022-05-18 14:00:56 +0900

    Organismの作成 ①organismフォルダとUserCardの作成 ②カードのAtomを作成 ③UserIcon＋名前のmoleculesを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { PrimaryButton } from "./components/button/PrimaryButton";
-import { SecondaryButton } from "./components/button/SecondaryButton";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  image: "https://source.unsplash.com/ZRZSmK362Xw",
+  name: "muto",
+  email: "11111@aaa.com",
+  phone: "999-9999-9998",
+  company: {
+    name: "Sample"
+  },
+  website: "https://www.google.co.jp/"
+};
 
 export const App = () => {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
-      <SearchInput></SearchInput>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 };
