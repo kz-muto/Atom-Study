9ed445c7c41b8e912111b1dde2b243c5f810e9f3
mutoch <89289398+mutoch@users.noreply.github.com>
2022-05-18 11:47:28 +0900

    Moleculesの作成 検索ボックスと検索ボタンを作成 ①divとinputとPrimaryButtonで作成 ②inputのAtomを作成 ③CSSを適用

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PrimaryButton } from "./components/button/PrimaryButton";
 import { SecondaryButton } from "./components/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 import "./styles.css";
 
 export const App = () => {
@@ -8,6 +9,7 @@ export const App = () => {
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <SearchInput></SearchInput>
     </div>
   );
 };
---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,25 @@
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/input/input";
+import styled from "styled-components";
+
+export const SearchInput = () => {
+  return (
+    <SContainer>
+      {/* placeholder:検索ボックスの裏に写す */}
+      <Input placeholder="あああ"></Input>
+      <SButtonWrapper>
+        {/* <input placeholder="検索条件を入力" /> */}
+        <PrimaryButton>検索</PrimaryButton>
+      </SButtonWrapper>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;
